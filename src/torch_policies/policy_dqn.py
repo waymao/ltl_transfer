3089a98ae0b5dee13171bca9c2784bdb0af7d362
waymao <my.waymao@gmail.com>
2023-10-18 13:18:30 -0400

    make dqn more concise

--- src/torch_policies/policy_dqn.py
+++ src/torch_policies/policy_dqn.py
@@ -116,7 +116,7 @@ class DQN(nn.Module):
         A = self.action_dim
         # q for current state
         q_all_values_NA = self.model(s1_NS) # Q values for all actions
-        q_values_N1 = torch.gather(q_all_values_NA, 1, a_N.view(-1, 1)) # selected Q
+        q_values_N = torch.gather(q_all_values_NA, 1, a_N.view(-1, 1)).squeeze() # selected Q
         
         # q for next state using target model.
         with torch.no_grad():
@@ -124,7 +124,6 @@ class DQN(nn.Module):
 
         # target and loss
         q_values_hat_N = r_N + self.gamma * max_q_values_next_N * ~terminated_N
-        q_values_N = torch.squeeze(q_values_N1)
         # dqn_loss = F.mse_loss(q_values_N, q_values_hat_N, reduction="mean")
         dqn_loss = 0.5 * torch.sum(torch.square(q_values_hat_N - q_values_N))
 
