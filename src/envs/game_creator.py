a82af396b83bd39647ed8e2207399c2c9f907bcf
waymao <my.waymao+git@gmail.com>
2023-11-18 19:45:58 -0500

    Do not load miniworld when not compatible

--- src/envs/game_creator.py
+++ src/envs/game_creator.py
@@ -1,11 +1,13 @@
-from .grid.game import Game as GridGame
-from .miniworld import NavigateEnv, MiniWorldLTLWrapper
-from typing import Union
+# from .grid.game import Game as GridGame
+# from .miniworld import NavigateEnv, MiniWorldLTLWrapper
+# from typing import Union
 
-def get_game(name, params) -> Union[GridGame, MiniWorldLTLWrapper]:
+def get_game(name, params):
     if name == "grid":
+        from .grid.game import Game as GridGame
         return GridGame(params)
     elif name == "miniworld":
+        from .miniworld import NavigateEnv, MiniWorldLTLWrapper
         env = NavigateEnv(params)
         env = MiniWorldLTLWrapper(env, params)
         return env         
