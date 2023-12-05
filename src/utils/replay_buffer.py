49fbc5a580419d0e19ee3f8ea9df0971587e0b31
waymao <my.waymao+git@gmail.com>
2023-12-05 15:55:05 -0500

    more efficient implementation of buffers

--- src/utils/replay_buffer.py
+++ src/utils/replay_buffer.py
@@ -13,32 +13,37 @@ class ReplayBuffer(object):
             Max number of transitions to store in the buffer. When the buffer
             overflows the old memories are dropped.
         """
-        self._storage = []
+        self._s1_storage = None
+        self._a_storage = None
+        self._s2_storage = None
+        self._next_goal_storage = None
         self._maxsize = size
         self._next_idx = 0
+        self.size = 0
 
     def __len__(self):
         return len(self._storage)
 
-    def add(self, s1, a, s2, next_goal):
-        data = (s1, a, s2, next_goal)
+    def add(self, s1: np.ndarray, a, s2: np.ndarray, next_goal: np.ndarray):
+        if self._s1_storage is None:
+            s_shape = s1.shape
+            a_shape = np.array(a).shape
+            next_goal_shape = next_goal.shape
+            self._s1_storage = np.zeros((self._maxsize, *s_shape))
+            self._s2_storage = np.zeros((self._maxsize, *s_shape))
+            self._a_storage = np.zeros((self._maxsize, *a_shape))
+            self._next_goal_storage = np.zeros((self._maxsize, *next_goal_shape))
 
-        if self._next_idx >= len(self._storage):
-            self._storage.append(data)
-        else:
-            self._storage[self._next_idx] = data
+        self._s1_storage[self._next_idx] = s1
+        self._a_storage[self._next_idx] = np.array(a)
+        self._s2_storage[self._next_idx] = s2
+        self._next_goal_storage[self._next_idx] = next_goal
         self._next_idx = (self._next_idx + 1) % self._maxsize
+        if self.size < self._maxsize:
+            self.size += 1
 
     def _encode_sample(self, idxes):
-        S1, A, S2, Goal = [], [], [], []
-        for i in idxes:
-            data = self._storage[i]
-            s1, a, s2, next_goal = data
-            S1.append(np.array(s1, copy=False))
-            A.append(np.array(a, copy=False))
-            S2.append(np.array(s2, copy=False))
-            Goal.append(np.array(next_goal, copy=False))
-        return np.array(S1), np.array(A), np.array(S2), np.array(Goal)
+        return self._s1_storage[idxes], self._a_storage[idxes], self._s2_storage[idxes], self._next_goal_storage[idxes]
 
     def sample(self, batch_size, random_samples=True):
         """Sample a batch of experiences.
@@ -63,7 +68,7 @@ class ReplayBuffer(object):
             the end of an episode and 0 otherwise.
         """
         if random_samples:
-            idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
+            idxes = np.random.choice(np.arange(self.size), size=batch_size, replace=False)
         else:
             idxes = range(len(self._storage) - batch_size, len(self._storage))
         return self._encode_sample(idxes)
