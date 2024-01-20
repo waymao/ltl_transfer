723cdf9d7da612662391f05249294155785d5486
waymao <my.waymao+git@gmail.com>
2024-01-20 06:23:43 -0500

    tianshou parallel training code

--- src/baseline_ts_state.py
+++ src/baseline_ts_state.py
@@ -1,6 +1,8 @@
 
 # %%
 import os
+
+from ts_policy_bank import create_discrete_sac_policy
 os.environ["PYGLET_HEADLESS"] = "true"
 os.environ["PYGLET_HEADLESS_DEVICE"] = "0"
 
@@ -19,10 +21,14 @@ import torch
 from torch import nn
 import numpy as np
 
+import time
+
 from torch.utils.tensorboard import SummaryWriter
 from tianshou.utils.logger.tensorboard import TensorboardLogger
 
 NUM_PARALLEL_JOBS = 4
+
+device = "cpu"
 
 # %%
 test_envs = get_game(name="miniworld_simp_no_vis", params=GameParams(
@@ -60,34 +66,19 @@ train_envs = get_game(name="miniworld_simp_no_vis", params=GameParams(
 #             target_update_interval=1000,
 #             learning_starts=10000,
 #             train_freq=1)
-class DummyPreProcess(nn.Module):
-    def __init__(self, output_dim=4):
-        super().__init__()
-        self.output_dim = output_dim
-
-    def forward(self, x, state):
-        return x, state
-state_dim = train_envs.observation_space.shape[0]
-preprocess_net = DummyPreProcess(output_dim=state_dim)
-actor = Actor(preprocess_net, 4, [256, 256, 256], device="cuda")
-actor_optim = Adam(actor.parameters(), lr=1e-4)
-critic1 = Critic(preprocess_net, hidden_sizes=[256, 256, 256], last_size=4, device="cuda")
-critic1_optim = Adam(critic1.parameters(), lr=1e-4)
-critic2 = Critic(preprocess_net, hidden_sizes=[256, 256, 256], last_size=4, device="cuda")
-critic2_optim = Adam(critic2.parameters(), lr=1e-4)
-
-policy = DiscreteSACPolicy(
-    actor, actor_optim, 
-    critic1, critic1_optim, 
-    critic2, critic2_optim,
-    alpha=0.03,
-    gamma=0.99
-).to("cuda")
 
 # logger
-run_name = input("run name: ")
+run_name = time.time()
 writer = SummaryWriter(f"results/tianshou/16-{run_name}/")
 logger = TensorboardLogger(writer)
+
+
+policy = create_discrete_sac_policy(
+    num_actions=test_envs.action_space.n, 
+    num_features=test_envs.observation_space.shape[0], 
+    hidden_layers=[256, 256, 256],
+    device=device
+)
 
 
 train_buffer = ReplayBuffer(int(1e6))
@@ -103,7 +94,8 @@ trainer = OffpolicyTrainer(
     batch_size=64, 
     update_per_step=1,
     step_per_collect=12,
-    logger=logger
+    logger=logger,
+    stop_fn=lambda x: x >= 9, # mean test reward
 )
 
 trainer.run()
---
+++ src/ts_policy_bank.py
@@ -0,0 +1,68 @@
+from typing import List, Mapping, Union
+
+from torch_policies.learning_params import LearningParameters, get_learning_parameters
+from tianshou.policy import BasePolicy, DiscreteSACPolicy
+from tianshou.data import Batch, ReplayBuffer
+from tianshou.policy import PPOPolicy, DiscreteSACPolicy, TD3Policy
+from tianshou.env import DummyVectorEnv, SubprocVectorEnv
+from tianshou.utils.net.common import ActorCritic
+from tianshou.utils.net.discrete import Actor, Critic
+from tianshou.trainer import OffpolicyTrainer
+from tianshou.data import Collector, ReplayBuffer
+from torch_policies.network import get_CNN_preprocess
+from torch.optim import Adam
+import torch
+from torch import nn
+
+class TianshouPolicyBank:
+    def __init__(self):
+        self.policies: List[BasePolicy] = []
+        self.policy2id: Mapping[Union[tuple, str], int] = {}
+    
+    def add_LTL_policy(self, ltl: Union[tuple, str], policy: BasePolicy):
+        if ltl not in self.policy2id:
+            self.policy2id[ltl] = len(self.policies)
+            self.policies.append(policy)
+    
+    def save(self, path):
+        policy_list = {
+            ltl: self.policies[id].state_dict() \
+                for ltl, id in self.policy2id.items()
+        }
+        torch.save(policy_list, path)
+    
+    def get_all_policies(self):
+        return {ltl: self.policies[id] for ltl, id in self.policy2id.items()}
+
+
+class DummyPreProcess(nn.Module):
+    def __init__(self, dim=4):
+        super().__init__()
+        self.output_dim = dim
+
+    def forward(self, x, state):
+        return x, state
+
+def create_discrete_sac_policy(
+    num_actions: int, 
+    num_features: int, 
+    hidden_layers: List[int] = [256, 256, 256],
+    learning_params: LearningParameters = get_learning_parameters("dsac", "miniworld_no_vis"), 
+    device="cpu"
+) -> DiscreteSACPolicy:
+    preprocess_net = DummyPreProcess(dim=num_features)
+    actor = Actor(preprocess_net, num_actions, hidden_layers, device=device)
+    actor_optim = Adam(actor.parameters(), lr=learning_params.pi_lr)
+    critic1 = Critic(preprocess_net, hidden_sizes=hidden_layers, last_size=num_actions, device=device)
+    critic1_optim = Adam(critic1.parameters(), lr=learning_params.lr)
+    critic2 = Critic(preprocess_net, hidden_sizes=hidden_layers, last_size=num_actions, device=device)
+    critic2_optim = Adam(critic2.parameters(), lr=learning_params.lr)
+
+    policy = DiscreteSACPolicy(
+        actor, actor_optim, 
+        critic1, critic1_optim, 
+        critic2, critic2_optim,
+        alpha=0.03,
+        gamma=0.99
+    ).to(device)
+    return policy
